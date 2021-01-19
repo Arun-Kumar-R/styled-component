5d1962c7cafcddce3279963a459ea3ab4a77ef31
srishtie <srishtie.tech@gmail.com>
2021-01-19 22:23:26 +0530

    remove router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 // Import Screens
 import Login from './Screens/login/login';
@@ -7,10 +6,10 @@ import Counter from './Screens/Counter/counter';
 
 const App = () => {
   return (
-    <Router>
-      <Route exact path = '/' component = {Login} />
-      <Route exact path = '/testing' component = {Counter} />
-    </Router>
+    <>
+      <Login />
+      <Counter />
+    </>
   );
 }
 
